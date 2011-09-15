9935ee7ac77b2cf70fee82ee06391552b2fdb532
regisfloret@gmail.com <regisfloret@gmail.com@600d64b2-3677-71f4-84df-818c3ffa534a>
2011-09-15 10:25:08 +0000

    Create setup.py script, changing switchscreenrc to switchscreenrc.sample

--- setup.py
+++ setup.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-print "TODO : setup.py"
+import shutil
+import os
+
+from distutils.core import setup
+
+import switchscreen
+
+shutil.copyfile("switchscreen.py", "switchscreen")
+
+setup(
+    name = "switchscreen",
+    version = switchscreen.__version__,
+    description = "",
+    author = u"Régis FLORET",
+    author_mail = "regisfloret@gmail.com",
+    url = "http://code.google.com/p/switchscreen/",
+    scripts = [
+        'switchscreen',
+    ]
+)
+
+os.remove("switchscreen")
